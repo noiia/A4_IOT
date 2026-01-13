00e28006d01bbe51e2b4e6469021d4c5cd7f51a4
noiia <edwin.lecomte31@gmail.com>
2026-01-13 15:06:52 +0100

    feat(add) : adding course list on home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:a4_iot/types/course_list.dart';
 import 'package:flutter/material.dart';
 import 'package:a4_iot/widget/profile_card.dart';
+import 'package:a4_iot/widget/course_list.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -9,50 +11,75 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       body: LayoutBuilder(
         builder: (context, constraints) {
-          if (constraints.maxWidth < 700) {
-            return _mobileLayout();
-          } else {
-            return _desktopLayout();
-          }
+          return _mobileLayout();
+          // if (constraints.maxWidth < 700) {
+          //   return _mobileLayout();
+          // } else {
+          //   return _desktopLayout();
+          // }
         },
       ),
     );
   }
 
   Widget _mobileLayout() {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.all(16),
         child: Center(
-          child: SizedBox(
-            width: 500,
-            child: ProfileCard(
-              firstName: "Edwin",
-              lastName: "Lecomte",
-              status: "Etudiant",
-              proms: "FISA A4",
-              campus: "Reims",
-              avatarUrl: "https://i.pravatar.cc/300",
-            ),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 500,
+                child: ProfileCard(
+                  firstName: "Edwin",
+                  lastName: "Lecomte",
+                  status: "Etudiant",
+                  proms: "FISA A4",
+                  campus: "Reims",
+                  avatarUrl: "https://i.pravatar.cc/300",
+                ),
+              ),
+              SizedBox(
+                width: 500,
+                height: 450,
+                child: CourseList(
+                  courses: [
+                    CourseListType(
+                      courseName: 'Course 1',
+                      instructor: 'Instructor 1',
+                      room: 'Room 1',
+                      schedule: 'Schedule 1',
+                    ),
+                    CourseListType(
+                      courseName: 'Course 1',
+                      instructor: 'Instructor 1',
+                      room: 'Room 1',
+                      schedule: 'Schedule 1',
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget _desktopLayout() {
-    return const Center(
-      child: SizedBox(
-        width: 500,
-        child: ProfileCard(
-          firstName: "Edwin",
-          lastName: "Lecomte",
-          status: "Etudiant",
-          proms: "FISA A4",
-          campus: "Reims",
-          avatarUrl: "https://i.pravatar.cc/300",
-        ),
-      ),
-    );
-  }
+  // Widget _desktopLayout() {
+  //   return const Center(
+  //     child: SizedBox(
+  //       width: 500,
+  //       child: ProfileCard(
+  //         firstName: "Edwin",
+  //         lastName: "Lecomte",
+  //         status: "Etudiant",
+  //         proms: "FISA A4",
+  //         campus: "Reims",
+  //         avatarUrl: "https://i.pravatar.cc/300",
+  //       ),
+  //     ),
+  //   );
+  // }
 }
---
+++ lib/widget/course_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:a4_iot/types/course_list.dart';
+
+class CourseList extends StatelessWidget {
+  final List<CourseListType> courses;
+  const CourseList({super.key, required this.courses});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: ListView.builder(
+        itemCount: courses.length,
+        itemBuilder: (context, index) {
+          final course = courses[index];
+          return ListTile(
+            title: Text(course.courseName),
+            subtitle: Text(
+              '${course.room} - ${course.instructor} - ${course.schedule}',
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
